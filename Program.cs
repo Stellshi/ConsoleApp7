fe9ee15edd7074944f6a0dc4a14e906503a09143
Stellshi <simplepro47@gmail.com>
2021-05-27 00:33:14 +0300

    Добавьте файлы проекта.

---
+++ Program.cs
@@ -0,0 +1,24 @@
+﻿using System;
+
+namespace ConsoleApp7
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Введите возраст вашей девушки: ");
+            string text = Console.ReadLine();
+            int years = Convert.ToInt32(text);
+            if (years > 16)
+            {
+                Console.WriteLine("Тогда все ОК");
+
+            }
+            else
+            {
+                Console.WriteLine("Мужик у тебя проблемы, я вызвал полицию");
+            }
+            Console.ReadKey();
+        }
+    }
+}
